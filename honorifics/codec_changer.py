dfeca3562cc2daf0566b3e0bc5edf7e03774cf44
Fadion96 <nowak196@gmail.com>
2018-11-13 11:45:25 +0100

    Delete duration, add -from and -to flag

--- honorifics/codec_changer.py
+++ honorifics/codec_changer.py
@@ -3,49 +3,53 @@ from typing import Dict, List, Tuple
 
 class CodecChanger(object):
     def __init__(self, input_file: str, output_file: str, debanding: bool, subtitles: str = None,
-                 timestamps: List[float] = None, durations: List[float] = None):
+                 timestamps: List[List[float]] = None, start: float = None, end: float = None):
         self.input = input_file
         self.output = output_file
         self.debanding = debanding
+        self.start = start
+        self.end = end
         self.subtitles = subtitles if timestamps is not None else ""
         self.timestamps = timestamps if timestamps is not None else []
-        self.durations = durations if durations is not None else []
 
     def __start_of_video(self):
-        return "\"[0:0]trim=start=0:duration=" + str(self.timestamps[0]) + "[a];", "[a]"
+        if self.start:
+            return "\"[0:0]trim=start=" + str(self.start) + ":end=" + str(self.timestamps[0][0]) + "[a];", "[a]"
+        else:
+            return "\"[0:0]trim=start=0:end=" + str(self.timestamps[0][0]) + "[a];", "[a]"
 
     def __negation_part(self, last_part: str, number: int) -> Tuple[str, str]:
-        result = "[0:0]trim=start=" + str(self.timestamps[number]) + ":duration=" + str(
-            self.durations[number]) + ",setpts=PTS-STARTPTS[trimmed" + str(
+        result = "[0:0]trim=start=" + str(self.timestamps[number][0]) + ":end=" + str(
+            self.timestamps[number][1]) + ",setpts=PTS-STARTPTS[trimmed" + str(
             number) + "];"
         result += "[trimmed" + str(number) + "]negate[negation" + str(number) + "];"
         result += last_part + "[negation" + str(number) + "]concat[negated" + str(number) + "];"
         return result, "[negated" + str(number) + "]"
 
     def __deband_noise_part(self, last_part: str, number: int) -> Tuple[str, str]:
-        result = "[0:0]trim=start=" + str(self.timestamps[number]) + ":duration=" \
-                 + str(self.durations[number]) + ",setpts=PTS-STARTPTS[trimmed" + str(number) + "];"
+        result = "[0:0]trim=start=" + str(self.timestamps[number][0]) + ":end=" \
+                 + str(self.timestamps[number][1]) + ",setpts=PTS-STARTPTS[trimmed" + str(number) + "];"
         result += "[trimmed" + str(number) + "]deband[debanded" + str(number) + "];"
         result += "[debanded" + str(number) + "]noise=alls=6:allf=t[noise" + str(number) + "];"
         result += last_part + "[noise" + str(number) + "]concat[debnoise" + str(number) + "];"
         return result, "[debnoise" + str(number) + "]"
 
     def __normal_part(self, last_part: str, number: int) -> Tuple[str, str]:
-        normal_timestamp = round(self.timestamps[number] + self.durations[number], 3)
-        normal_duration = round(self.timestamps[number + 1] - normal_timestamp, 3)
-        result = "[0:0]trim=start=" + str(normal_timestamp) + ":duration=" + str(
-            normal_duration) + ",setpts=PTS-STARTPTS[nor" + str(
+        result = "[0:0]trim=start=" + str(self.timestamps[number][1]) + ":end=" + str(
+            self.timestamps[number + 1][0]) + ",setpts=PTS-STARTPTS[nor" + str(
             number) + "];"
         result += last_part + "[nor" + str(number) + "]concat[normal" + str(number) + "];"
         return result, "[normal" + str(number) + "]"
 
     def __end_of_video(self, last_part: str) -> str:
         last_index = len(self.timestamps) - 1
-        last_timestamp = round(self.timestamps[last_index] + self.durations[last_index], 3)
-        result = "[0:0]trim=start=" + str(last_timestamp) + ",setpts=PTS-STARTPTS[ending];"
+        if self.end:
+            result = "[0:0]trim=start=" + str(self.timestamps[last_index][1]) + ":end=" + str(self.end) + ",setpts=PTS-STARTPTS[ending];"
+        else:
+            result = "[0:0]trim=start=" + str(self.timestamps[last_index][1]) + ",setpts=PTS-STARTPTS[ending];"
         if self.subtitles:
             result += last_part + "[ending]concat[subs];"
-            result += "[subs]ass=" + self.subtitles.replace(".\\", "") + "[out1]\" "
+            result += "[subs]ass=" + self.subtitles + "[out1]\" "
         else:
             result += last_part + "[ending]concat[out1]\" "
         return result
@@ -69,9 +73,16 @@ class CodecChanger(object):
             first_pass += filter_complex
             second_pass += filter_complex
         else:
+            if self.start:
+                first_pass += " -ss " + str(self.start)
+                second_pass += " -ss " + str(self.start)
+            if self.end:
+                first_pass += " -to " + str(self.end)
+                second_pass += " -to " + str(self.end)
             if self.subtitles:
-                first_pass += " -vf \"ass=" + self.subtitles.replace(".\\", "") + "\" "
-                second_pass += " -vf \"ass=" + self.subtitles.replace(".\\", "") + "\" "
+                first_pass += " -vf \"ass=" + self.subtitles + "\" "
+                second_pass += " -vf \"ass=" + self.subtitles + "\" "
+
         first_pass += " -c:v libvpx-vp9 -pass 1 -pix_fmt yuv420p -crf 21 -threads 3 -speed 4 -tile-columns 6 -frame-parallel 1 -b:v 0 -an -f matroska /dev/null"
         second_pass += " -c:v libvpx-vp9 -pass 2 -pix_fmt yuv420p -crf 21 -threads 3 -speed 1 -tile-columns 6 -frame-parallel 1 -b:v 0 -auto-alt-ref 1 -lag-in-frames 25 -an -f matroska " + self.output
         command = first_pass + second_pass
@@ -93,6 +104,11 @@ class CodecChanger(object):
             ending = self.__end_of_video(last_part)
             filter_complex += ending[:-2] + ";[out1]scale=-1:360[out2]\" -map [out2]"
             command += filter_complex
+        else:
+            if self.start:
+                command += " -ss " + str(self.start)
+            if self.end:
+                command += " -to " + str(self.end)
         command += " -c:v libx264 -preset ultrafast " + self.output
         return command
 
--- scripts/changing_codec.py
+++ scripts/changing_codec.py
@@ -10,13 +10,13 @@ from honorifics.codec_changer import CodecChanger
 def parse_arg():
     parser = argparse.ArgumentParser(description="Codec changer with negate filter at the given timestamps")
     parser.add_argument("input_video", type=str, help="Input video")
-    parser.add_argument("timestamp", type=str, help="Timestamp in seconds", nargs='*')
-    parser.add_argument("-duration", "-d", nargs='*', type=str, help="Duration of negation in seconds (default = 10)",
-                        default=[])
+    parser.add_argument("-timestamp", "-t", type=str, action='append', metavar=('start', 'end'), help="Timestamp in seconds", nargs=2)
     parser.add_argument("-output", "-o", metavar='output', nargs="?", type=str, help="Output video",
                         default="output.mkv")
     parser.add_argument("-subtitles", "-s", metavar="subtitles", type=str, help="subtitles to be hardsubbed")
     parser.add_argument("-deband", "-de", action='store_true')
+    parser.add_argument("-f", "-from", type=str, nargs=1, metavar='start', help="Video start time")
+    parser.add_argument("-to", type=str, nargs=1, metavar='end', help="Video end time")
     return parser.parse_args()
 
 
@@ -29,20 +29,27 @@ def hms_to_seconds(timestamp):
 
 if __name__ == '__main__':
     args = parse_arg()
-    times = []
-    durations = []
-    if len(args.timestamp) > len(args.duration):
-        diff = len(args.timestamp) - len(args.duration)
-        args.duration.extend(["10"] * diff)
-    for s in args.timestamp:
-        seconds = hms_to_seconds(s)
-        times.append(seconds)
-    for d in args.duration:
-        duration_seconds = hms_to_seconds(d)
-        durations.append(duration_seconds)
+    timestamps = []
+    if args.timestamp:
+        for s in args.timestamp:
+            times = []
+            for x in s:
+                seconds = hms_to_seconds(x)
+                times.append(seconds)
+            timestamps.append(times)
+    print(timestamps)
+    if args.f:
+        start = hms_to_seconds(args.f[0])
+    else:
+        start = 0
+    if args.to:
+        end = hms_to_seconds(args.to[0])
+    else:
+        end = 0
+    subs = args.subtitles.replace('\\', '\\\\\\\\').replace(':', '\\\\:')
     changer = CodecChanger(args.input_video, args.output,
-                           args.deband, subtitles=args.subtitles,
-                           timestamps=times, durations=durations)
+                           args.deband, subtitles=subs,
+                           timestamps=timestamps, start=start, end=end)
     command = changer.make_command()
     print("\n", command, "\n")
     os.system(command)
